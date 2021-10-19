d77cd26fde7d411647381280594222d92eccccc3
Maksym Khorkanin <71780594+ppolyanskiyy@users.noreply.github.com>
2021-10-19 14:30:45 +0300

    Add concatenateByDelimeter function

--- C++/Include.h
+++ C++/Include.h
@@ -12,3 +12,20 @@ ReturnType applyParams(Params&& params, ReturnType(*function)(Args...))
 }
 
 
+template< typename ... Args >
+std::string concatenateByDelimeter(const std::string delimeter, Args const& ... args)
+{
+    std::ostringstream stream;
+    using List = int[];
+
+    /*
+    This call creates a (compile-time) recursive initialization list construction to create an unused, unnamed, dummy int array,
+    but the expression ( (void)(stream << args), 0 ) has the side-effect of streaming the args into the stream (before returning the second 0)
+    */
+    (void)List
+    {
+        0, ((void)(stream << args << delimeter), 0) ...
+    };
+
+    return stream.str();
+}
